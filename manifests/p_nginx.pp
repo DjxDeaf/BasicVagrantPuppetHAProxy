c95dbbba95bc9588720667767c72efb85110838d
DjxDeaf <eliranbz@gmail.com>
2014-07-05 00:10:57 +0300

    minor changes

--- manifests/p_nginx.pp
+++ manifests/p_nginx.pp
@@ -1,3 +1,4 @@
+#The reason I've set 'default' is because every time a node is created, puppet should assume it's another Nginx server
 node default {
 class { 'nginx': }
 #Erasing the default index page and replacing it with Hello World
